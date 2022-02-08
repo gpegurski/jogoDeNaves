1cc46b0af1ae2b0aa284b636d3d43a663df417b4
gpegurski <grazipegurski@gmail.com>
2022-02-08 18:52:48 -0300

    criacao funcoes loop e moveFundo

--- js/js.js
+++ js/js.js
@@ -7,6 +7,23 @@ function start() {
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
-}
-
 //$ é usado por conta do JQuery
+
+let jogo = {}//Game Loop
+
+	jogo.timer = setInterval(loop,30);
+	
+	function loop() {
+	
+	movefundo();
+	
+	} 
+	
+	
+	function movefundo() {//Função que movimenta o fundo do jogo
+	
+	esquerda = parseInt($("#fundoGame").css("background-position"));
+	$("#fundoGame").css("background-position",esquerda-1);
+	
+	}
+}
