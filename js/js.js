26c165029825ee8b729e219f9616bbd25ae7481f
gpegurski <grazipegurski@gmail.com>
2022-02-08 18:25:29 -0300

    alteracao css e criacao funcao start no arquivo js

---
+++ js/js.js
@@ -0,0 +1,12 @@
+function start() {
+
+	$("#inicio").hide();
+	
+	$("#fundoGame").append("<div id='jogador'></div>");
+	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='inimigo2'></div>");
+	$("#fundoGame").append("<div id='amigo'></div>");
+
+}
+
+//$ é usado por conta do JQuery
