381b41f085c851e563faccafb67876e4c7113dc6
gpegurski <grazipegurski@gmail.com>
2022-02-09 11:28:01 -0300

    limitando a movimentacao do helicoptero dentro da div

--- js/js.js
+++ js/js.js
@@ -52,18 +52,26 @@ let TECLA = {
 		if (jogo.pressionou[TECLA.W]) {
 			var topo = parseInt($("#jogador").css("top"));
 			$("#jogador").css("top",topo-10);
+
+			if (topo<=0) {
+		
+				$("#jogador").css("top",topo+10);
+			}
 		
 		}
 		
 		if (jogo.pressionou[TECLA.S]) {
 			
 			var topo = parseInt($("#jogador").css("top"));
-			$("#jogador").css("top",topo+10);	
+			$("#jogador").css("top",topo+10);
+
+			if (topo>=434) {	
+				$("#jogador").css("top",topo-10);
+					
+			}
 		}
 		
-		if (jogo.pressionou[TECLA.D]) {
-			
-			//Chama função Disparo	
+		if (jogo.pressionou[TECLA.D]) {//Chama função Disparo	
 		}
 	
 		}
